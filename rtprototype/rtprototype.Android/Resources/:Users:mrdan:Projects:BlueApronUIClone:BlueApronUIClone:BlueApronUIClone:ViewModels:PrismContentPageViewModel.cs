7d976e5b9826a2faab5ef6dcdadf93413402036f
Daniel Andujar <daniel.ap252@gmail.com>
2019-03-27 16:03:45 -0400

    Initial load

---
+++ rtprototype/rtprototype.Android/Resources/:Users:mrdan:Projects:BlueApronUIClone:BlueApronUIClone:BlueApronUIClone:ViewModels:PrismContentPageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Prism.Commands;
+using Prism.Navigation;
+using Prism.Logging;
+using Prism.Services;
+
+namespace BlueApronUIClone.ViewModels
+{
+    public class PrismContentPageViewModel : ViewModelBase
+    {
+        public PrismContentPageViewModel(INavigationService navigationService) : base(navigationService)
+        {
+        }
+    }
+}
